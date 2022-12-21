97d9b1baa608b92a856a62172d6a40136b6715b5
Yassine <yassine@indexifi.io>
2022-12-21 09:49:33 +0000

    connect to metamask and get balance

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { Web3ReactProvider } from '@web3-react/core'
+import { Web3Provider } from '@ethersproject/providers'
+
+function getLibrary(provider: any): Web3Provider {
+  const library = new Web3Provider(provider)
+  library.pollingInterval = 12000
+  return library
 }
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <Component {...pageProps} />
+    </Web3ReactProvider>
+  )
+}
+
+export default MyApp
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,51 @@
 import type { NextPage } from 'next'
 import styles from '../styles/Home.module.css'
+import { useEffect, useState } from 'react'
+
+import { useWeb3React } from '@web3-react/core'
+import { Web3Provider } from '@ethersproject/providers'
+import { InjectedConnector } from '@web3-react/injected-connector'
+
+const ConnectWallet = () => {
+  const injectedConnector = new InjectedConnector({ supportedChainIds: [1, 3, 4, 5, 42,], })
+  const { chainId, account, activate, active, library } = useWeb3React<Web3Provider>()
+  const onClick = () => {
+    activate(injectedConnector)
+  }
+  const [balance, setBalance] = useState("")
+
+  useEffect(() => {
+    console.log(chainId, account, active)
+    console.log(library?.getBalance(String(account)).then((result) => {
+      setBalance(String(result / 1e18))
+    }))
+  },);
+
+  return (
+    <div>
+      <div>ChainId: {chainId}</div>
+      <div>Account: {account}</div>
+      {active ? (
+        <div>✅ </div>
+      ) : (
+        <button type="button" onClick={onClick}>
+          Connect Connect
+        </button>
+      )}
+      <div>Balance of Account: {balance}</div>
+    </div>
+  )
+}
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div >
       <main className={styles.main}>
-        <h2>
-          Welcome to playground
-        </h2>
+        <h2 >Welcome to playground</h2>
+        <ConnectWallet />
       </main>
     </div>
   )
 }
+
 export default Home
